face60b003fe427322af2d73e3277ed9fb2ade6a
yangtao <69585768+ytanck@users.noreply.github.com>
2024-05-23 17:49:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const app= http.createServer(function (request, response) {
     var time = moment().format('MMMM Do YYYY, h:mm:ss a');
     console.log(request.url);
     // 内容类型: text/plain。并用charset=UTF-8解决输出中文乱码
-    response.writeHead(200, {'Content-Type': 'text/plain; charset=UTF-8'});
- 
-    // 下句是发送响应数据
+    response.writeHead(200, {
+      "Content-Type": "text/plain; charset=UTF-8",
+      "Access-Control-Allow-Origin": "*",
+    });
     response.end('Hello World! 这是简单的web服务器test!\n'+time);
 }).listen(3000);
 // 终端打印如下信息
